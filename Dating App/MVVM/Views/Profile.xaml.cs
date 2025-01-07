0315e111426733f470c8342d928cd8babc1c6ec1
Ravi <150629030+TheRealSpaceBaker@users.noreply.github.com>
2025-01-07 14:31:00 +0100

    Made the Profile Page

--- Dating App/MVVM/Views/Profile.xaml.cs
+++ Dating App/MVVM/Views/Profile.xaml.cs
@@ -1,15 +1,19 @@
 using Dating_App.MVVM.Views.Authentication;
 using Dating_App.MVVM.Models;
 using Dating_App.MVVM.Models.Data;
+using Dating_App.MVVM.ViewModels;
 
 namespace Dating_App.MVVM.Views;
 
 public partial class Profile : ContentPage
 {
     private DatingRegistry _db = new DatingRegistry();
+    private ProfileViewModel ProfileView = new ProfileViewModel();
     public Profile()
     {
         InitializeComponent();
+
+        BindingContext = ProfileView;
     }
 
     private void OnLogOutClicked(object sender, EventArgs e)
